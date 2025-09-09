bf9035fe146e55ddb67e95a581a0f322e8d78d1d
eduardoezequieel <riv.edu10@gmail.com>
2025-09-08 21:29:24 -0600

    feat: add bootstrap and design for log in

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,23 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/Desafio1_LIS_2025/resources/css/globals.css">
+	<link rel="stylesheet" href="/Desafio1_LIS_2025/resources/css/login.css">
+	<link href="/Desafio1_LIS_2025/resources/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+	<title>Document</title>
 </head>
+
 <body>
-	<h2>Iniciar sesión</h2>
-	<form id="login-form">
-		<div>
-      <label for="username">Usuario</label>
-      <input type="text" name="username" id="username-input">
-		</div>
-		<div>
-			<label for="password">Contraseña</label>
-      <input type="password" name="password" id="password-input">
-		</div>
-    <button type="submit">Iniciar sesión</button>
-	</form>
+	<div class="d-flex flex-column align-items-center justify-content-center vh-100">
+		<h2 class="heading text-center">Iniciar sesión</h2>
+		<form id="login-form">
+			<div class="mb-3">
+				<label for="username" class="form-label">Usuario</label>
+				<input type="text" name="username" id="username-input" class="form-control">
+			</div>
+			<div class="mb-4">
+				<label for="password" class="form-label">Contraseña</label>
+				<input type="password" name="password" id="password-input" class="form-control">
+			</div>
+			<div class="d-grid gap-2">
+				<button class="btn btn-primary" type="button">Iniciar sesión</button>
+			</div>
+		</form>
+	</div>
+	<script src="/Desafio1_LIS_2025/resources/lib/bootstrap/bootstrap.bundle.min.js"></script>
 	<script src="/Desafio1_LIS_2025/resources/js/auth/login.js"></script>
 </body>
+
 </html>
